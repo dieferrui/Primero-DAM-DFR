5bfcdaa8747fbf49d5e0a28bd2bd69d274c4f823
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-10-27 12:23:38 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Scripts sueltos Java/BilleteTren.java

--- Scripts sueltos Java/BilleteTren.java
+++ Scripts sueltos Java/BilleteTren.java
@@ -7,12 +7,12 @@ public class BilleteTren {
         double precioViaje = 0d;
         double precioFinal = 0d;
 
+        System.out.print("Introduzca la distancia (en km) entre el origen y destino: ");
+        int distancia = Integer.parseInt(System.console().readLine());
+
         do {
             System.out.println("Introduzca su tipo de billete:\n1. Sencillo.\n2. Ida y vuelta.");
             tipoBillete = Integer.parseInt(System.console().readLine());
-
-            System.out.print("Introduzca la distancia (en km) entre el origen y destino: ");
-            int distancia = Integer.parseInt(System.console().readLine());
 
             switch (tipoBillete) {
                 case 1: 
@@ -26,9 +26,10 @@ public class BilleteTren {
             }
         } while (tipoBillete != 1 && tipoBillete != 2);
 
-        System.out.println("¿Posee usted tarjeta interraíl?\n1. Sí.\n2. No.");
-        tarjeta = Integer.parseInt(System.console().readLine());
-
+        do {
+            System.out.println("¿Posee usted tarjeta interraíl?\n1. Sí.\n2. No.");
+            tarjeta = Integer.parseInt(System.console().readLine());
+        } while (tarjeta != 1 && tarjeta != 2);
 
         if (tarjeta == 1) {
             precioFinal = precioViaje * 0.75;
