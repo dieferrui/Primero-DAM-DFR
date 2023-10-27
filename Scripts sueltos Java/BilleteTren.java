2a71cdf81057756a1ba8f6228115534e4431ddfa
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-10-27 12:07:28 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Scripts sueltos Java/BilleteTren.java

--- Scripts sueltos Java/BilleteTren.java
+++ Scripts sueltos Java/BilleteTren.java
@@ -2,7 +2,6 @@ public class BilleteTren {
 
     public static void main(String[] args) {
 
-        double precioBillete = 0d;
         int tipoBillete = 0;
         double precioViaje = 0d;
         int tarjeta = 0;
@@ -12,29 +11,24 @@ public class BilleteTren {
             System.out.println("Introduzca su tipo de billete:\n1. Sencillo.\n2. Ida y vuelta.");
             tipoBillete = Integer.parseInt(System.console().readLine());
 
+            System.out.print("Introduzca la distancia (en km) entre el origen y destino: ");
+            int distancia = Integer.parseInt(System.console().readLine());
+
             switch (tipoBillete) {
                 case 1: 
-                    precioBillete = 0.33d; 
+                    precioViaje = 0.33d * distancia; 
                     break;
                 case 2: 
-                    precioBillete = 0.26d; 
+                    precioViaje = 0.26d * 2 * distancia; 
                     break;
                 default: 
                     System.out.println("Tipo de billete inválido.");
             }
         } while (tipoBillete != 1 && tipoBillete != 2);
 
-        System.out.print("Introduzca la distancia (en km) entre el origen y destino: ");
-        int distancia = Integer.parseInt(System.console().readLine());
-        
-        if (tipoBillete == 1) {
-            precioViaje = distancia * precioBillete;
-        } else {
-            precioViaje = distancia * 2 * precioBillete;
-        }
-
         System.out.println("¿Posee usted tarjeta interraíl?\n1. Sí.\n2. No.");
         tarjeta = Integer.parseInt(System.console().readLine());
+
 
         if (tarjeta == 1) {
             precioFinal = precioViaje * 0.75;
