8106c73a32f2425b2639059a7fdd06f4e5e7213b
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-11-14 13:33:24 +0100

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   Scripts sueltos Java/CantidadesReceta.java

---
+++ Scripts sueltos Java/CantidadesReceta.java
@@ -0,0 +1,20 @@
+public class CantidadesReceta {
+
+    public static void main(String[] args) {
+
+        final int GR_COLIFLOR = 100;
+        final double CEBOLLETAS = 1 / 6d;
+        final double ZANAHORIAS = 3 / 6d;
+        final double POLLO = 500 / 6d;
+        final double PINYONES = 25 / 6d;
+        final double PASAS = 75 / 6d;
+        final double CALDO = 220 / 6d;
+        final double CURRY = 1.5 / 6d;
+
+        System.out.println("Indique para cuántas personas será la receta: ");
+        int comensales = Integer.parseInt(System.console().readLine());
+
+        
+
+    }
+}
