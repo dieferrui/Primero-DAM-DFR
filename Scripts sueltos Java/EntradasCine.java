ef9f86b935bcba4e653907d4abd7b5767bbdb43e
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-11-17 11:25:41 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   Scripts sueltos Java/CalculoPrimos.java
    	new file:   Scripts sueltos Java/EntradasCine.java

---
+++ Scripts sueltos Java/EntradasCine.java
@@ -0,0 +1,74 @@
+public class EntradasCine {
+
+    public static void main(String[] args) {
+        
+        boolean checked = false;
+        int entradas = 0;
+        int dia = 0;
+        int elComandanteFidel = 0;
+        
+        while (!checked) {
+
+            try {
+                System.out.println("Permitame hacer algunas preguntas antes de empezar:");
+        
+                System.out.println("Para cuantas personas quiere adquirir entradas?");
+                entradas = Integer.parseInt(System.console().readLine());
+
+                System.out.println("Para que dia de la semana desea entradas?\nIntroduzca el numero de dia del 1 al 7:");
+                dia = Integer.parseInt(System.console().readLine());
+
+                System.out.println("Por ultimo, indique si posee tarjeta de fidelidad:\n 1. Si\n 2. No");
+                elComandanteFidel = Integer.parseInt(System.console().readLine());
+
+                checked = true;
+
+            } catch (NumberFormatException e) {
+                System.out.println("El valor introducido no es valido: debe introducir un numero entre la seleccion disponible.");
+
+            }
+            
+        }
+
+        System.out.println("Gracias, sus respuestas se estan procesando...");
+        CalculaPrecio(entradas, dia, elComandanteFidel);
+    }
+
+    public static void CalculaPrecio(int entradas, int dia, int elComandanteFidel) {
+        
+        int precioBase = 8;
+        double precioFinal = 0d;
+
+        if (dia == 3) {
+            precioBase = 5;
+            
+            if (elComandanteFidel == 1) {
+                precioFinal = (precioBase * entradas) * 0.9;
+
+            } else {
+                precioFinal = precioBase * entradas;
+
+            }
+        
+        } else if (dia == 4 && entradas == 2) {
+            if (elComandanteFidel == 1) {
+                precioFinal = 11 * 0.9;
+
+            } else {
+                precioFinal = 11;
+
+            }
+
+        } else {
+            if (elComandanteFidel == 1) {
+                precioFinal = (precioBase * entradas) * 0.9;
+
+            } else {
+                precioFinal = precioBase * entradas;
+
+            }
+        }
+
+        System.out.printf("El precio de sus entradas es de %.2f euros.", precioFinal);
+    }
+}
