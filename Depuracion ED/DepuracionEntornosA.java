4282a210c561047c571d5b036db35670db5d83db
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-11-29 10:10:30 +0100

    On branch Depuracion  Your branch is up to date with 'origin/Depuracion'.
     Changes to be committed:
    	modified:   Depuracion ED/DepuracionEntornosA.java

--- Depuracion ED/DepuracionEntornosA.java
+++ Depuracion ED/DepuracionEntornosA.java
@@ -10,7 +10,13 @@ public class DepuracionEntornosA {
         // JavaApplication13();
         // JavaApplication14();
         // JavaApplication15();
-        JavaApplication16();
+        // JavaApplication16();
+        // JavaApplication17();
+        // JavaApplication18();
+        // JavaApplication19();
+        // JavaApplication20();
+        // JavaApplication21();
+        JavaApplication22();
         
     }
 
@@ -229,5 +235,214 @@ public class DepuracionEntornosA {
         }
 
         entrada.close();
+    }
+
+    public static void JavaApplication17() {
+        
+        int num, i, aux, cont, cifra;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Introduce numero entero positivo: ");
+            num = sc.nextInt();
+            if (num <= 0) {
+                System.out.println("Numero no valido.");
+            }
+        } while (num <= 0);
+
+        aux = num;
+        cont = 0; //variable para contar las cifras del número
+       
+        while (aux != 0) {
+            aux = aux / 10;
+            cont++;
+        }
+
+        //obtener cada una de las cifras comenzando por la izquierda 
+        
+        for (i = cont - 1; i >= 0; i--) {
+            //obtenemos la cifra de la izquierda
+            cifra = num / (int) Math.pow(10, i);
+            //mostramos su nombre
+            switch (cifra) {
+                case 0:
+                    System.out.print("cero ");
+                    break;
+                case 1:
+                    System.out.print("uno ");
+                    break;
+                case 2:
+                    System.out.print("dos ");
+                    break;
+                case 3:
+                    System.out.print("tres ");
+                    break;
+                case 4:
+                    System.out.print("cuatro ");
+                    break;
+                case 5:
+                    System.out.print("cinco ");
+                    break;
+                case 6:
+                    System.out.print("seis ");
+                    break;
+                case 7:
+                    System.out.print("siete ");
+                    break;
+                case 8:
+                    System.out.print("ocho ");
+                    break;
+                case 9:
+                    System.out.print("nueve ");
+                    break;
+            }
+            
+            if (i != 0) { //si no es la última cifra
+                System.out.print(" - ");
+                num = num % (int) Math.pow(10, i);
+            }
+        }
+        
+        System.out.println();
+        System.out.println("Fin de programa");
+        sc.close();
+    }
+
+    public static void JavaApplication18() {
+        
+        Scanner sc = new Scanner(System.in);
+        int n1, n2;
+
+        System.out.print("Introduzca primer número: ");
+        n1 = sc.nextInt();
+        System.out.print("Introduzca segundo número: ");
+        n2 = sc.nextInt();
+
+        if (n1 % 10 != n2 % 10) {
+            System.out.println("Los números acaban con cifras distintas");
+        } else {
+            System.out.println("Los dos números acaban con la misma cifra");
+        }
+
+        sc.close();
+
+    }
+
+    public static void JavaApplication19() {
+        int N;
+        double factorial;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Introduce un número > 0: ");
+            N = sc.nextInt();
+        } while (N < 0);
+
+        for (int i = 0; i <= N ; i++) {
+            factorial = 1;
+
+            for (int j = 1; j <= i; j++) {
+                factorial = factorial * j;
+            }
+            
+            System.out.printf("%2d! = %.0f %n", i, factorial);
+        }
+
+        sc.close();
+    }
+
+    public static void JavaApplication20() {
+        
+        int i, suma = 0, n1, n2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce primer número: ");
+        n1 = sc.nextInt();
+        System.out.print("Introduce segundo número: ");
+        n2 = sc.nextInt();
+
+        for (i = 1; i < n1; i++) {
+            if (n1 % i == 0) {
+                suma = suma + i;
+            }
+        }
+
+        if (suma == n2) {
+            suma = 0;
+            for(i = 1; i < n2; i++) {
+                if(n2 % i == 0) {
+                    suma = suma + i;
+                }
+            }
+            
+            if(suma != n1) {
+                System.out.println("No son Amigos");
+            } else {
+                System.out.println("Son amigos");
+            }
+
+        } else {
+            System.out.println("No son amigos");
+        }
+
+        sc.close();
+    }
+
+    public static void JavaApplication21() {
+        
+        Scanner sc = new Scanner(System.in);
+        int i, N;
+        int contMas = 0, contMenos = 0;
+        double media = 0;
+
+        do {
+            System.out.print("Número de personas: ");
+            N = sc.nextInt();
+        } while (N <= 0);
+
+        double[] alto = new double[N];
+
+        System.out.println("Lectura de la altura de las personas: ");
+        for (i = 0; i < N; i++) {
+            System.out.print("Persona " + (i + 1) + " = ");
+            alto[i] = sc.nextDouble();
+            media = media + alto[i];
+        }
+        
+        media = media / N;
+
+        for (i = 0; i < N; i++) {
+            if (alto[i] > media) {
+                contMas++;
+        } else if (alto[i] < media) {
+            contMenos++;
+        }
+
+        }
+        System.out.println("Estatura media: " + media);
+        System.out.println("Personas con estatura superior a la media: " + contMas);
+        System.out.println("Personas con estatura inferior a la media: " + contMenos);
+
+        sc.close();
+    }
+
+    public static void JavaApplication22() {
+        
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    for (int l = 0; l <= 9; l++) {
+                        for (int m = 0; m <= 9; m++) {
+                            System.out.print(i != 3 ? i : "E");
+                            System.out.print(j != 3 ? j : "E");
+                            System.out.print(k != 3 ? k : "E");
+                            System.out.print(l != 3 ? l : "E");
+                            System.out.print(m != 3 ? m : "E");
+                            System.out.println();
+                        }
+                    }
+                }
+            }
+        }  
     }
 }
